4726ce31d45ed00681e421562579e9f8150ea372
Ilya Petrov <ilya.muromec@gmail.com>
2012-08-06 23:53:10 +0300

    handle connection ids and msg routing

--- redqueue/ws.py
+++ redqueue/ws.py
@@ -1,3 +1,6 @@
+from uuid import uuid4 as uuid
+import json
+
 from tornado.httpserver import HTTPServer
 from tornado.web import Application, StaticFileHandler
 from tornado.websocket import WebSocketHandler
@@ -6,26 +9,32 @@ from redqueue import task
 
 class WSTask(task.Task):
     key = 'task:ws'
-    connections = []
+    connections = {}
     def on_data(self, data):
-        print 'task data', data
-        for c in self.connections:
-            c.write_message(data)
+        c = self.connections.get(data['_id'])
+        if not c:
+            return
+
+        c.write_message(json.dumps(data['payload']))
 
 task.runnable_tasks.append(WSTask)
 
 class Handler(WebSocketHandler):
     def open(self):
         print "New connection opened."
-        WSTask.connections.append(self)
+        self._id = str(uuid())
+        self.write_message(self._id)
+
+        WSTask.connections[self._id] = self
+        print WSTask.connections
 
     def on_message(self, message):
         print message
-        self.write_message(u"You said: " + message)
-
+        #self.write_message(u"You said: " + message)
 
     def on_close(self):
         print "Connection closed."
+        WSTask.connections.pop(self._id, None)
 
 
 class Server(HTTPServer):
